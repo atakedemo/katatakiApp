bebf84477b5378815c6726821a4f890c166f510b
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2022-01-03 11:39:14 +0900

    Amplifyコンポーネント・認証機能追加

--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+      region: "ap-northeast-1",
+      userPoolId: "ap-northeast-1_2cOGpMkHb",
+      userPoolWebClientId: "553b8kmrb5ib0du39kvju8u7aq"
+  }
+});
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import MainRouter from './routes';
+import { withAuthenticator } from 'aws-amplify-react-native'
 /*
 export default function App() {
   return <MainRouter />;
 }*/
 
 type Props = {};
-export default class App extends Component<Props>{
+//export default class App extends Component<Props>{
+class App extends Component<Props>{
   render() {
     return <MainRouter />
   }
 }
+
+export default withAuthenticator(App);
