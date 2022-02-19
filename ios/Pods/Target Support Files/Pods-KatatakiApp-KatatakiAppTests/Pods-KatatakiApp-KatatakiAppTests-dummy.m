3cd64808df66c9e4e5ff61d13d560819f410aba1
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2022-02-19 16:44:20 +0900

    iOSでのビルド調整

---
+++ ios/Pods/Target Support Files/Pods-KatatakiApp-KatatakiAppTests/Pods-KatatakiApp-KatatakiAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KatatakiApp_KatatakiAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_KatatakiApp_KatatakiAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-KatatakiApp/Pods-KatatakiApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KatatakiApp : NSObject
+@end
+@implementation PodsDummy_Pods_KatatakiApp
+@end
